45d41011c5ee71a7dcb2cacb4d8ed826cbf5beb8
Leon11-dot <trendworldmarket@gmail.com>
2026-09-12 12:06:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pizza-de-silva-v20260912-hours-v3';
+const CACHE='pizza-de-silva-v20260912-hours-v4';
 self.addEventListener('install',event=>self.skipWaiting());
 self.addEventListener('activate',event=>{
   event.waitUntil((async()=>{
